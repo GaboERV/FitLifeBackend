1de63b0a18ebd45d700ec5f0785e966e06d5690e
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-10-02 00:58:06 -0500

    feat: añadiendo seguirdad a datos sensibles

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import routerLogin from './Router/LoginRoutes';
 import routerUser from './Router/UserRoutes';
-
 
 const app = express();
 
--- src/infrastructure/Repositories/connection.ts
+++ src/infrastructure/Repositories/connection.ts
@@ -1,10 +1,10 @@
 import mysql, { Pool } from "mysql2/promise";
 
 const pool: Pool = mysql.createPool({
-  host: "localhost",
-  user: "root",      
-  password: "58875887", 
-  database: "Fitlife",       
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,     
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
